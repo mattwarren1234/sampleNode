138263b593b97bdf78aa37fba99ab1d0cf68a676
Matt Warren <mattwarrenmail@gmail.com>
2014-10-21 18:56:49 -0700

    fixed bug where node was expecting a templating engine

--- server.js
+++ server.js
@@ -28,10 +28,6 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
     });
 }
 
@@ -39,10 +35,6 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
 });
 
 var port = process.env.PORT || 8080; 
